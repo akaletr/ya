763715b9bdc37ec5db2c05e8a86378310aaa996d
Dmitry Polyakov <d.poliakov@bpm-cons.ru>
2022-06-19 20:31:01 +0400

    1-3 Небольшие изменения

--- internal/app/app.go
+++ internal/app/app.go
@@ -30,7 +30,7 @@ func (app *app) GetURL(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Location", long)
 	w.WriteHeader(http.StatusTemporaryRedirect)
-	//_, err = w.Write([]byte(long))
+	_, err = w.Write([]byte(long))
 	if err != nil {
 		fmt.Println(err)
 	}
