67974cdce0a1f8dbfc2b75a3884cf1e7ffe21e01
Dmitry Polyakov <d.poliakov@bpm-cons.ru>
2022-06-21 12:25:58 +0400

    2-6 добавлено хранилище в файле

--- internal/app/app.go
+++ internal/app/app.go
@@ -35,7 +35,7 @@ func (app *app) GetURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Location", long)
-	//w.WriteHeader(http.StatusTemporaryRedirect)
+	w.WriteHeader(http.StatusTemporaryRedirect)
 	fmt.Println("99999", long)
 	_, err = w.Write([]byte(long))
 	if err != nil {
