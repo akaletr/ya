4c5f8bbf36f24c869414ad6c089bf211d0748926
Dmitry Polyakov <d.poliakov@bpm-cons.ru>
2022-06-27 19:32:15 +0400

    Рефакторинг

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"cmd/shortener/main.go/internal/app"
 	"cmd/shortener/main.go/internal/config"
-	"fmt"
 	"log"
 )
 
 func main() {
 	cfg, err := config.GetConfig()
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 	myApp := app.New(cfg)
