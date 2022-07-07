625a58721ccf656b55ff036a53d2507a61bcf30b
Dmitry Polyakov <d.poliakov@bpm-cons.ru>
2022-07-07 13:26:39 +0300

    3-11 рефакторинг

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -21,27 +21,30 @@ func (p postgresDatabase) Read(value string) (string, error) {
 			log.Println(err)
 		}
 	}()
-	rows, err := db.Query("select long from data where short=? ", value)
+
+	str := fmt.Sprintf("select long from data where short=%s", value)
+	rows, err := db.Query(str)
 	if err != nil {
 		return "", err
 	}
 
-	// обязательно закрываем перед возвратом функции
-	defer rows.Close()
+	defer func() {
+		err = rows.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
 
 	// пробегаем по всем записям
-	l := ""
+	long := ""
 	for rows.Next() {
-		rows.Scan(&l)
-		fmt.Println(l)
+		err = rows.Scan(&long)
+		if err != nil {
+			return "", err
+		}
 	}
 
-	// проверяем на ошибки
-	err = rows.Err()
-	if err != nil {
-		return "", err
-	}
-	return l, nil
+	return long, nil
 }
 
 func (p postgresDatabase) Write(id, key, value string) error {
@@ -56,15 +59,16 @@ func (p postgresDatabase) Write(id, key, value string) error {
 		}
 	}()
 
-	_, err = db.Exec("create table data (id varchar(30), short varchar(30), long varchar(130))")
+	// если таблицы нет - создаем
+	_, err = db.Exec("create table data (id varchar(30), short varchar(60), long text")
 	if err != nil {
 		fmt.Println(err)
 	}
 	str := fmt.Sprintf("insert into data values (%s, '%s', '%s')", id, key, value)
-	//db.Exec("insert into data values (?, ?, ?)", id, key, value)
 	_, err = db.Exec(str)
 	if err != nil {
 		fmt.Println(err)
+		return err
 	}
 	return nil
 }
@@ -81,43 +85,32 @@ func (p postgresDatabase) ReadAll(id string) (map[string]string, error) {
 		}
 	}()
 
-	str := fmt.Sprintf("select * from data where id='%s'", id)
+	str := fmt.Sprintf("select short, long from data where id='%s'", id)
 	rows, err := db.Query(str)
-	//rows, err := db.Query("select * from data where id=? ", id)
 	if err != nil {
 		fmt.Println(err)
 		return map[string]string{}, err
 	}
 
-	// обязательно закрываем перед возвратом функции
-	defer rows.Close()
+	defer func() {
+		err = db.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
 
-	l := map[string]string{}
+	result := map[string]string{}
 	for rows.Next() {
-		var id1, short, long string
-		err := rows.Scan(&id1, &short, &long)
+		var short, long string
+		err := rows.Scan(&short, &long)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		l[short] = long
+		result[short] = long
 	}
 
-	// пробегаем по всем записям
-
-	//for rows.Next() {
-	//	var short, long string
-	//	rows.Scan(&short, &long)
-	//	l[short] = long
-	//	fmt.Println(l)
-	//}
-
-	// проверяем на ошибки
-	err = rows.Err()
-	if err != nil {
-		return map[string]string{}, err
-	}
-	return l, nil
+	return result, nil
 }
 
 func NewPostgresDatabase(connectionString string) Storage {
