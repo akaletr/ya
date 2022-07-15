7f5578f19255073481a31a246a26c5c5771648a3
Dmitry Polyakov <d.poliakov@bpm-cons.ru>
2022-07-15 16:06:31 +0300

    3-13 добавлена реализация методов

--- internal/storage/fileStorage.go
+++ internal/storage/fileStorage.go
@@ -15,7 +15,7 @@ type fileStorage struct {
 }
 
 func (fs fileStorage) Read(value string) (string, error) {
-	file, err := os.OpenFile(fs.path, os.O_CREATE, 0777)
+	file, err := os.OpenFile(fs.path, os.O_RDONLY|os.O_CREATE, 0777)
 	if err != nil {
 		return "", err
 	}
@@ -44,13 +44,13 @@ func (fs fileStorage) Write(id, key, value string) error {
 		_ = file.Close()
 	}()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		d := scanner.Text()
-		if strings.Split(d, "|")[0] == key {
-			return NewError(CONFLICT, "conflict")
-		}
-	}
+	//scanner := bufio.NewScanner(file)
+	//for scanner.Scan() {
+	//	d := scanner.Text()
+	//	if strings.Split(d, "|")[0] == key {
+	//		return NewError(CONFLICT, "conflict")
+	//	}
+	//}
 	log.Println(fs.path)
 	data := fmt.Sprintf("%s|%s|%s\n", key, value, id)
 	_, err = file.Write([]byte(data))
